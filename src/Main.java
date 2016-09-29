9dbf02eebcf68e4c830abf751554d6a33ee90461
dongjin1204 <dongjin_1204@naver.com>
2016-09-29 14:27:10 +0900

    cc

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		new Controllers();
-
 		Controllers.getMusicController().requestSelectAllMusic();
-<<<<<<< HEAD
-		Controllers.getMenuController().requestShowMenu();
-=======
-
-=======
-		Controllers.getMusicController().requestSelectAllMusic();
-<<<<<<< HEAD
 		Controllers.getUserController().requestRegisterUser();
->>>>>>> refs/remotes/yuns111/master
-=======
 		Controllers.getMenuController().requestShowMenu();
->>>>>>> refs/remotes/yuns111/master
->>>>>>> refs/remotes/origin/my
+		
 	}
 
 }
