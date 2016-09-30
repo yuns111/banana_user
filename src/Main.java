471fafab5ec1b49632ee87dd3a31c4ee37c8257f
gonomujasic <gonomujasic@hotmail.com>
2016-09-30 16:43:05 +0900

    dd
    dd

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import banana_user.controller.Controllers;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println("우하하\tt랄라라");
 		new Controllers();
 		Controllers.getMenuController().requestShowMenu();
 	}
+
 
 }
